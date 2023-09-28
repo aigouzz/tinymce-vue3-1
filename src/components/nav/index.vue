1d8ece7b2aa9152b14db2afa739dc162c5bac6d8
gxc <1003823149@qq.com>
2023-09-28 11:02:23 +0800

    feat: add nav component

---
+++ src/components/nav/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="index_nav">
+    <div
+      class="index_nav_c"
+      v-for="(item, index) in list"
+      :key="item.title"
+      @click="goto(index)"
+    >
+      {{ item.title }}
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      list: [
+        {
+          title: "首页",
+          path: "/",
+        },
+        {
+          title: "我的",
+          path: "/my",
+        },
+      ],
+    };
+  },
+  methods: {
+    goto(index) {
+      this.$router.push(this.list[index].path);
+    },
+  },
+};
+</script>
+<style lang="scss">
+.index_nav {
+  width: 100%;
+}
+</style>
